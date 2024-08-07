3e5bb9b25c1fc80386d42cfdb78e8c2bccb46a0d
BalakrishnanShekar <153483181+BalakrishnanShekar@users.noreply.github.com>
2024-08-07 21:51:26 +0530

    Add files via upload

---
+++ l1.js
@@ -0,0 +1,16 @@
+function hello()
+{
+    alert("Have a good day");
+}
+
+function time()
+{
+    var now=new Date();
+    alert(now);
+}
+
+function bye()
+{
+    var umm="Have a nice day";
+    alert(umm);
+}
